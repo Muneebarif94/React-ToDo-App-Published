245bce7f9ff1524465afcfcfd70cc528dae7a565
Muneeb Arif <smuneebarif@gmail.com>
2020-03-11 03:23:16 +0100

    Updates

---
+++ precache-manifest.da038bfa508cd5c7d273206c4177a150.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e0ad539351267bc28eebb27272d85a",
+    "url": "/React-ToDo-App-Published/index.html"
+  },
+  {
+    "revision": "92f7679500a89d557fb0",
+    "url": "/React-ToDo-App-Published/static/css/main.1e754155.chunk.css"
+  },
+  {
+    "revision": "2052a60120e06a19bc8d",
+    "url": "/React-ToDo-App-Published/static/js/2.be88d37a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-ToDo-App-Published/static/js/2.be88d37a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92f7679500a89d557fb0",
+    "url": "/React-ToDo-App-Published/static/js/main.9fb0111e.chunk.js"
+  },
+  {
+    "revision": "744c38dc3cde365a7920",
+    "url": "/React-ToDo-App-Published/static/js/runtime-main.8d8eae48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-ToDo-App-Published/static/media/logo.5d5d9eef.svg"
+  }
+]);
